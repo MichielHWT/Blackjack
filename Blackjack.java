4cf16d5a3028b036794bef10b9b1ebb4414c7a52
MichielHWT <michielherpers@gmail.com>
2020-03-10 12:09:10 +0100

    Blackjack eerste commit

---
+++ Blackjack.java
@@ -0,0 +1,25 @@
+//package nl.workingtalent.blackjack
+
+import java.util.Scanner;
+
+public class Blackjack{
+
+	public static void main(String[] args){
+		
+		//Declare variables
+		Scanner textInput = new Scanner();
+		
+		//Ask how many players want to players
+		
+		
+		
+		//Create new player objects
+	
+		//Declare array with cards
+		
+		//Randomise array
+		
+		//
+		
+	}
+}
